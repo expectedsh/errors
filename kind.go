f7068d010863ddfe2ad3aafbb59119c256909102
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-07-17 21:01:11 +0200

    errors: add kind

--- kind.go
+++ kind.go
@@ -6,52 +6,52 @@ type Kind string
 
 const (
 	// KindCanceled indicates the operation was cancelled (typically by the caller).
-	KindCanceled Kind = "CANCELED"
+	KindCanceled Kind = "canceled"
 
 	// KindUnknown error. For example when handling errors raised by APIs that do not
 	// return enough error information.
-	KindUnknown Kind = "UNKNOWN"
+	KindUnknown Kind = "unknown"
 
 	// KindInvalidArgument indicates client specified an invalid argument. It
 	// indicates arguments that are problematic regardless of the state of the
 	// system (i.e. a malformed file name, required argument, number out of range,
 	// etc.).
-	KindInvalidArgument Kind = "INVALID_ARGUMENT"
+	KindInvalidArgument Kind = "invalid_argument"
 
 	// KindDeadlineExceeded means operation expired before completion. For operations
 	// that change the state of the system, this error may be returned even if the
 	// operation has completed successfully (timeout).
-	KindDeadlineExceeded Kind = "DEADLINE_EXCEEDED"
+	KindDeadlineExceeded Kind = "deadline_exceeded"
 
 	// KindNotFound means some requested entity was not found.
-	KindNotFound Kind = "NOT_FOUND"
+	KindNotFound Kind = "not_found"
 
 	// AlreadyExists means an attempt to create an entity failed because one
 	// already exists.
-	KindAlreadyExists Kind = "ALREADY_EXISTS"
+	KindAlreadyExists Kind = "already_exists"
 
 	// KindPermissionDenied indicates the caller does not have permission to execute
 	// the specified operation. It must not be used if the caller cannot be
 	// identified (Unauthenticated).
-	KindPermissionDenied Kind = "PERMISSION_DENIED"
+	KindPermissionDenied Kind = "permission_denied"
 
 	// KindUnauthenticated indicates the request does not have valid authentication
 	// credentials for the operation.
-	KindUnauthenticated Kind = "UNAUTHENTICATED"
+	KindUnauthenticated Kind = "unauthenticated"
 
 	// KindResourceExhausted indicates some resource has been exhausted, perhaps a
 	// per-user quota, or perhaps the entire file system is out of space.
-	KindResourceExhausted Kind = "RESOURCE_EXHAUSTED"
+	KindResourceExhausted Kind = "resource_exhausted"
 
 	// KindFailedPrecondition indicates operation was rejected because the system is
 	// not in a state required for the operation's execution. For example, doing
 	// an rmdir operation on a directory that is non-empty, or on a non-directory
 	// object, or when having conflicting read-modify-write on the same resource.
-	KindFailedPrecondition Kind = "FAILED_PRECONDITION"
+	KindFailedPrecondition Kind = "failed_precondition"
 
 	// KindAborted indicates the operation was aborted, typically due to a concurrency
 	// issue like sequencer check failures, transaction aborts, etc.
-	KindAborted Kind = "ABORTED"
+	KindAborted Kind = "aborted"
 
 	// KindOutOfRange means operation was attempted past the valid range. For example,
 	// seeking or reading past end of a paginated collection.
@@ -63,31 +63,35 @@ const (
 	// We recommend using OutOfRange (the more specific error) when it applies so
 	// that callers who are iterating through a space can easily look for an
 	// OutOfRange error to detect when they are done.
-	KindOutOfRange Kind = "OUT_OF_RANGE"
+	KindOutOfRange Kind = "out_of_range"
 
 	// KindUnimplemented indicates operation is not implemented or not
 	// supported/enabled in this service.
-	KindUnimplemented Kind = "UNIMPLEMENTED"
+	KindUnimplemented Kind = "unimplemented"
 
 	// KindInternal errors. When some invariants expected by the underlying system
 	// have been broken. In other words, something bad happened in the library or
 	// backend service. Do not confuse with HTTP Internal Server Error; an
 	// Internal error could also happen on the client code, i.e. when parsing a
 	// server response.
-	KindInternal Kind = "INTERNAL"
+	KindInternal Kind = "internal"
 
 	// KindUnavailable indicates the service is currently unavailable. This is a most
 	// likely a transient condition and may be corrected by retrying with a
 	// backoff.
-	KindUnavailable Kind = "UNAVAILABLE"
+	KindUnavailable Kind = "unavailable"
 
 	// DataLoss indicates unrecoverable data loss or corruption.
-	KindDataLoss Kind = "DATA_LOSS"
+	KindDataLoss Kind = "data_loss"
 
 	// KindNone is the zero-value, is considered an empty error and should not be
 	// used.
 	KindNone Kind = ""
 )
+
+var KindToStatusCodeHook = func(k Kind) int {
+	return http.StatusInternalServerError
+}
 
 func (k Kind) ToStatusCode() int {
 	switch k {
@@ -119,5 +123,5 @@ func (k Kind) ToStatusCode() int {
 		return http.StatusServiceUnavailable
 	}
 
-	return http.StatusInternalServerError
+	return KindToStatusCodeHook(k)
 }
